b937a3d9f9e43e7cc831a285b7d2a7ef34b8337f
Dmytro Lytovchenko <dmytro.lytovchenko@erlang-solutions.com>
2018-09-24 16:34:46 +0200

    Installation should continue even if Rust not found

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
 
-setup(name="term",
-      version="1.0",
-      rust_extensions=[RustExtension("term.native_codec_impl",
-                                     binding=Binding.RustCPython)],
-      packages=["term"],
-      # rust extensions are not zip safe, just like C-extensions.
-      zip_safe=False,
-      )
+VERSION = '1.0'
+PKGNAME = "term"
+DESCR = 'Erlang term and External Term Format codec in Python and native Rust extension'
+AUTHOR = 'Erlang Solutions Ltd and S2HC Sweden AB',
+AUTHOR_EMAIL = 'dmytro.lytovchenko@gmail.com,pyrlang@s2hc.com',
+
+try:
+    from setuptools_rust import Binding, RustExtension
+
+    setup(name=PKGNAME,
+          version=VERSION,
+          description=DESCR,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          rust_extensions=[RustExtension("term.native_codec_impl",
+                                         binding=Binding.RustCPython)],
+          packages=[PKGNAME],
+          # rust extensions are not zip safe, just like C-extensions.
+          zip_safe=False)
+except:
+    print("----------------------------")
+    print("Rust Setuptools not found, continue with Python slow implementation only")
+    print("----------------------------")
+
+    setup(name=PKGNAME,
+          version=VERSION,
+          description=DESCR,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          packages=[PKGNAME])
