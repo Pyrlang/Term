2ca3480e2f2f8b98f180d7c78724d21aadb76266
sharyari <sharyari@gmail.com>
2020-11-26 14:21:18 +0100

    Use map_err and return CodecError instead of unwrap

--- src/errors.rs
+++ src/errors.rs
@@ -46,6 +46,8 @@ pub enum CodecError {
   AtomTooLong,
   #[fail(display="Float value {} is not finite", f)]
   NonFiniteFloat { f: f64 },
+  #[fail(display="IOError: {}", txt)]
+  IOError { txt: String }
 }
 
 pub type CodecResult<T> = Result<T, CodecError>;
