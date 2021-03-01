107a19ad7363c8119f7996073470dd36c21d5e8e
Johan Sommerfeld <johan@s2hc.com>
2021-03-01 07:43:10 +0100

    some workflow changes and version bump prepping for release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.2'
+VERSION = '1.3'
 PKGNAME = "pyrlang-term"
 MODULENAME = "term"
 DESCR = 'Erlang term and External Term Format codec in Python and native Rust extension'
