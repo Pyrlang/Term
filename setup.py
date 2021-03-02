865cf90c1ab0316913e53376d6578d5f5836a213
Johan Sommerfeld <johan@s2hc.com>
2021-03-02 15:06:17 +0100

    added some meta to package, bumped verison

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup
 
-VERSION = '1.3'
+VERSION = '1.4'
 PKGNAME = "pyrlang-term"
 MODULENAME = "term"
 DESCR = 'Erlang term and External Term Format codec in Python and native Rust extension'
 AUTHOR = 'Erlang Solutions Ltd and S2HC Sweden AB'
 AUTHOR_EMAIL = 'dmytro.lytovchenko@gmail.com, pyrlang@s2hc.com'
+URL = "https://github.com/Pyrlang/Term"
+
+with open("README.md", "r", encoding='utf-8') as fp:
+    LONG_DESCRPTION = fp.read()
+LONG_DESCRPTION_CONTENT_TYPE = "text/markdown"
 
 try:
     from setuptools_rust import Binding, RustExtension
 
     setup(name=PKGNAME,
           version=VERSION,
+          url=URL,
           description=DESCR,
+          long_description=LONG_DESCRPTION,
+          long_description_content_type=LONG_DESCRPTION_CONTENT_TYPE,
           author=AUTHOR,
           author_email=AUTHOR_EMAIL,
           rust_extensions=[RustExtension("term.native_codec_impl",
@@ -28,7 +36,10 @@ except Exception as e:
 
     setup(name=PKGNAME,
           version=VERSION,
+          url=URL,
           description=DESCR,
+          long_description=LONG_DESCRPTION,
+          long_description_content_type=LONG_DESCRPTION_CONTENT_TYPE,
           author=AUTHOR,
           author_email=AUTHOR_EMAIL,
           packages=[MODULENAME])
