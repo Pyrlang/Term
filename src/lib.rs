8de4ff948e556e4084eaf8bf2d77bb89055aeda5
Benedikt Reinartz <filmor@gmail.com>
2022-02-18 08:15:24 +0100

    Ignore incorrect lint

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(unused_braces)]
 #[macro_use] extern crate failure;
 //#[macro_use]
 //#[macro_use] extern crate lazy_static;
